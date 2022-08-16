28163f6dba3d0e3db65ac9d23e30edad484bcb73
Luis Andrés <luisandres33bolanos@gmail.com>
2022-08-16 19:05:33 +0200

    configured the connection to the database

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import database from './config/database.js';
+
+
 const app = express();
 
-app.listen(4000, () => {
-    console.log('servidor');
+// dotenv
+dotenv.config();
+
+// connection database
+database();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`port: ${PORT}`);
 })
