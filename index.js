0be3fcdcaccf5e68802df45466785a68e3c5a211
Luis Andrés <luisandres33bolanos@gmail.com>
2022-08-16 19:11:38 +0200

    installed the rest of the packages

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import helmet from "helmet";
 import database from './config/database.js';
 
 
 const app = express();
+
+// helmet
+app.use(helmet());
 
 // dotenv
 dotenv.config();
